3f037be1139fef8b2ac267749e72d16fd173166b
Muhammad Saif <saif.1077@yahoo.com>
2018-12-26 21:18:26 +0500

    Typo fixes port to PORT

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose =  require('mongoose')
 
 mongoose.Promise = global.Promise;
 let URI = ''
-if(process.env.port){
+if(process.env.PORT){
 URI = 'mongodb://saifimran1:Todo-api1@ds143594.mlab.com:43594/todo-api'
 }else{
  URI = 'mongodb://localhost:27017/TodoApp'   
