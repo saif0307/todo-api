66abb8f432bcc82108633af7008124ec795b2485
Muhammad Saif <saif.1077@yahoo.com>
2018-12-27 23:46:22 +0500

    Added patch route/TODOS

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
+const _ = require('lodash')
+
 const {mongoose} = require('./db/mongoose.js')
 const {Todo} =  require('./models/todos.js')
 const {User} =  require('./models/user.js')
@@ -64,6 +66,28 @@ app.delete('/todos/:id', (req, res) => {
         if(err) {
             res.status(400)
         }
+    })
+})
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send({
+            Error: 'Id is invalid'
+        })
+    }
+    let body = _.pick(req.body, ["text", "completed"])
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime()
+    }else {
+        body.completed = false
+        body.completedAt = null
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+         return res.status(404).send({})
+        }
+        res.status(200).send({todo})
     })
 })
 
